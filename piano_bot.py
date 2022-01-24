53c1669d09e64fde0dd08899c993a56863ec8ffc
Aik Khachatrian <aik.khachatrian@gmail.com>
2022-01-24 10:25:27 +0200

    update

--- piano_bot.py
+++ piano_bot.py
@@ -3,13 +3,14 @@ import pyautogui, time
 from selenium import webdriver
 
 
-user = input('Write your user name:\n')
+# user = input('Write your user name:\n')
 
-driver = webdriver.Chrome(executable_path='C:\\Users\\{}\\Downloads\\chromedriver_win32\\chromedriver.exe'.format(user))
-driver.get("https://www.yad.com/Magic-Tiles-3-Online")
-driver.maximize_window()
+# driver = webdriver.Chrome(executable_path='C:\\Users\\{}\\Downloads\\chromedriver_win32\\chromedriver.exe'.format(user))
+# driver.get("https://www.yad.com/Magic-Tiles-3-Online")
+# driver.maximize_window()
 
 amount = 1
+descent = 0
 first_20 = True
 
 y = 610
@@ -17,7 +18,7 @@ x = 770
 width = 1
 height = 1
 
-mouse_cors = [(i, y) for i in range(x, 1146, 125)]
+mouse_cors = [(i, y + descent) for i in range(x, 1146, 125)]
 
 monitors = [{"top": y, "left": x, "width": width, "height": height},
             {"top": y, "left": x + 125, "width": width, "height": height},
@@ -25,7 +26,6 @@ monitors = [{"top": y, "left": x, "width": width, "height": height},
             {"top": y, "left": x + 375, "width": width, "height": height}]
 
 limits = (range(150, 220), range(140, 200), range(190, 260))
-interval = 0.1
 
 def inLimits(*rgb):
     for i in range(3):
@@ -43,15 +43,12 @@ def screenShot():
 
 pyautogui.alert('Skip ad, maximize piano screen and click <OK> if you ready')
 
-while True:
+while amount < 2500:
     index = 1
     
     if amount % 20 == 0:
-        if first_20:
-            interval = interval*2/10
-            first_20 = False
-        else:
-            interval = interval**2/10
+        descent += 2
+        mouse_cors = [(i, y + descent) for i in range(x, 1146, 125)]
 
     pixelss = screenShot()
 
@@ -62,7 +59,4 @@ while True:
                 amount += 1
                 continue
         index += 1
-            
-    
-    time.sleep(interval)
     
